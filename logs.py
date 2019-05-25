02f526376a33c198d72d34bab48a3a91c19aa2a0
Brendon Smith <br3ndonland@protonmail.com>
2019-05-25 19:08:54 -0400

    Autoformat with Black and refactor docstrings
    https://black.readthedocs.io/

--- logs.py
+++ logs.py
@@ -1,58 +1,58 @@
-#!/usr/bin/env python3
+"""
+SQL database logs analysis project
+Udacity Full Stack Developer Nanodegree program
+Brendon Smith https://github.com/br3ndonland/udacity-fsnd-sql-logs
+"""
 
-# Udacity database logs analysis project
-
-# Import the psycopg2 module to work with PostgreSQL
 import psycopg2
 
-# Store the database name as an object for easy reference in functions
-DBNAME = 'news'
 
-
-# Create a helper function to re-use code shared among the three SQL queries
 def get_query_results(query):
-    """Helper function for code shared among the three SQL queries."""
-    # Connect to database
-    db = psycopg2.connect(database=DBNAME)
-    # Create a cursor object to run queries and scan through results
+    """Helper function for code shared among the three SQL queries.
+    ---
+    - Connect to database
+    - Create a cursor object to run queries and scan results
+    - Execute SQL query using cursor
+    - Fetch all results from cursor object
+    - Save as results object
+    - Close connection
+    """
+    db = psycopg2.connect(database="news")
     c = db.cursor()
-    # Execute the SQL query using the cursor
     c.execute(query)
-    # Fetch all results from the cursor object, and save as a results object
     result = c.fetchall()
-    # Close connection
     db.close()
     return result
 
 
-# 1. Most popular three articles
 def popular_articles():
-    """Returns a sorted list of the three most highly accessed articles,
-    with the top article first.
+    """1. Most popular three articles
+    ---
+    - Save SQL query as object
+    - Execute SQL query using helper function
+    - Print results, with top article first
     """
-    # Save SQL query as object
-    query = ("""
+    query = """
         select title, num from
             (select substr(path, 10), count(*) as num from log
             where path !='/' group by path)
         as hits, articles where substr = slug order by num desc limit 3;
-        """)
-    # Execute SQL query using the helper function
+        """
     result = get_query_results(query)
-    # Print results
-    print('\n', 'Query 1: Most popular three articles')
+    print("\n", "Query 1: Most popular three articles")
     for title, num in result:
-        print('    {}  --  {} views'.format(title, num))
+        print("    {}  --  {} views".format(title, num))
     pass
 
 
-# 2. Most popular authors
 def popular_authors():
-    """Returns a sorted list of the most popular article authors,
-    with the most popular author at the top.
+    """2. Most popular authors
+    ---
+    - Save SQL query as object
+    - Execute SQL query using helper function
+    - Print a sorted list of the most popular article authors
     """
-    # Save SQL query as object
-    query = ("""
+    query = """
         select name, sum(views) as total_views from
             (select name, author, title, views from
                 (select substr(path, 10), count(*) as views from log
@@ -61,23 +61,22 @@ def popular_authors():
                 where substr = slug and author = authors.id
                 order by views desc)
             as threetables group by name order by total_views desc;
-        """)
-    # Execute SQL query using the helper function
+        """
     result = get_query_results(query)
-    # Print results
-    print('\n', 'Query 2: Most popular authors')
+    print("\n", "Query 2: Most popular authors")
     for name, total_views in result:
-        print('    {}  --  {} views'.format(name, total_views))
+        print("    {}  --  {} views".format(name, total_views))
     pass
 
 
-# 3. Days on which >1% of HTTP requests led to errors
 def errors():
-    """Returns a list of days on which >1% of HTTP requests resulted in
-    HTTP error codes.
+    """3. Days on which >1% of HTTP requests led to errors
+    ---
+    - Save SQL query as object
+    - Execute SQL query using helper function
+    - Print days on which 1% or more HTTP requests returned errors
     """
-    # Save SQL query as object
-    query = ("""
+    query = """
         select errdate, http_requests, http_404,
         100.0 * http_404 / http_requests as errpct from
             (select date_trunc('day', time) as reqdate, count(*)
@@ -90,11 +89,9 @@ def errors():
         where reqdate = errdate
         and errors.http_404 > 0.01 * requests.http_requests
         order by errdate desc;
-        """)
-    # Execute SQL query using the helper function
+        """
     result = get_query_results(query)
-    # Print results
-    print('\n', 'Query 3: Days on which >1% HTTP requests returned 404 errors')
+    print("\n", "Query 3: Days on which >1% HTTP requests returned 404 errors")
     for errdate, http_requests, http_404, errpct in result:
         print("    {:%B %d, %Y}  --  {:.2f}% errors".format(errdate, errpct))
     pass
